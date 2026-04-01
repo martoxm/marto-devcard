61fc21e3fb737a2c658fd87a9789c0325acce8e1
Gabriel Martorelli <gabriel.martorelli@hotmail.com>
2026-03-31 23:44:50 -0300

    updatre name photo

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/foto perfil light.png")
+    img.setAttribute("src", "./assets/foto-perfil-light.png")
   } else {
-    img.setAttribute("src", "./assets/foto perfil.png")
+    img.setAttribute("src", "./assets/foto-perfil.png")
   }
 }
